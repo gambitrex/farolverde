ae61c8490ccd01eccba18bbf27a4ba17fbe05e96
Cereso Odisi Rodrigues <cereso_rodrigues@hotmail.com>
2014-03-22 12:56:51 -0300

    Classes parse

---
+++ FarolVerde/Models/Google/Coordinate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FarolVerde.Models.Google
+{
+    public class Coordinate
+    {
+        public string lat { get; set; }
+        public string lng { get; set; }
+    }
+}
