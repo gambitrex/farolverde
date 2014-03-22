3390fedb62f564590fa36b04545f71df546e01d8
Cereso Odisi Rodrigues <cereso_rodrigues@hotmail.com>
2014-03-22 12:26:38 -0300

    Correções server e adição de rota

---
+++ FarolVerde/Models/DTO/RouteRQ.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace FarolVerde.Models.DTO
+{
+    [DataContract]
+    public class RouteRQ
+    {
+        [DataMember]
+        public Point Origin { get; set; }
+        [DataMember]
+        public Point Destination { get; set; }
+        [DataMember]
+        public string DepartureTime { get; set; }
+        [DataMember]
+        public string ArrivalTime { get; set; }
+    }
+}
