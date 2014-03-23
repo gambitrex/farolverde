24291017a91be854dc3757f923bb759511f6bebd
Cereso Odisi Rodrigues <cereso_rodrigues@hotmail.com>
2014-03-23 00:23:01 -0300

    Adição de método no usuário.

--- FarolVerde/Models/User.cs
+++ FarolVerde/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FarolVerde.Models.DataBase;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -15,13 +16,18 @@ namespace FarolVerde.Models
         [DataMember]
         public bool Active { get; set; }
         [DataMember]
-        public string FaceBookId { get; set; }
+        public string FacebookId { get; set; }
         [DataMember]
         public string Name { get; set; }
         [DataMember]
         public string Username { get; set; }
         [DataMember]
         public Gender Gender { get; set; }
+
+        public void Save()
+        {
+            DBManager<Context>.Save(this);
+        }
     }
 
     public enum Gender
